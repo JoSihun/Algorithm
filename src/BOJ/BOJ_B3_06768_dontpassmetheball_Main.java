8533cffb79ca0811a74e44dd374c7457a53c6733
조시훈 <kepler452k@naver.com>
2024-02-23 17:46:49 +0900

    feat: 240222 Algorithm Practice

---
+++ src/BOJ/BOJ_B3_06768_dontpassmetheball_Main.java
@@ -0,0 +1,38 @@
+package BOJ;
+
+import java.io.*;
+
+public class BOJ_B3_06768_dontpassmetheball_Main {
+    private static int N;
+    private static int answer;
+
+    public static void combination(int cnt, int start, int[] arr) {
+        if (cnt == 3) {
+            if (arr[0] < arr[1] && arr[1] < arr[2])
+                answer++;
+            return;
+        }
+
+        for (int i = start; i < N - 1; i++) {
+            arr[cnt] = i;
+            combination(cnt + 1, i + 1, arr);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        answer = 0;
+        if (N >= 4) {
+            combination(0, 0, new int[3]);
+        }
+
+        sb.append(answer);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
