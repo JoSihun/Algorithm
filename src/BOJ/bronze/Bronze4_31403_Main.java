94a1aad0db69b4831ed2fecf7c02284aca4021df
조시훈 <kepler452k@naver.com>
2024-08-09 14:38:47 +0900

    feat: 240809 Algorithm Practice

---
+++ src/BOJ/bronze/Bronze4_31403_Main.java
@@ -0,0 +1,27 @@
+package BOJ.bronze;
+
+import java.io.*;
+
+public class Bronze4_31403_Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String A = br.readLine();
+        String B = br.readLine();
+        String C = br.readLine();
+        int a = Integer.parseInt(A);
+        int b = Integer.parseInt(B);
+        int c = Integer.parseInt(C);
+
+        int answer1 = a + b - c;
+        int answer2 = Integer.parseInt(A + B) - c;
+        sb.append(answer1).append("\n");
+        sb.append(answer2).append("\n");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
