1d3325ee74c21f59e1108178360b1a80d192bf48
조시훈 <kepler452k@naver.com>
2024-08-17 16:59:19 +0900

    feat: 240817 Algorithm Practice

---
+++ src/baekjoon/topology_sort/Main_G3_02623_음악프로그램.java
@@ -0,0 +1,62 @@
+package baekjoon.topology_sort;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_G3_02623_음악프로그램 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < N + 1; i++)
+            graph.add(new ArrayList<>());
+
+        int[] indegree = new int[N + 1];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int K = Integer.parseInt(st.nextToken());
+
+            int[] singers = new int[K];
+            for (int j = 0; j < K; j++)
+                singers[j] = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < K - 1; j++) {
+                int a = singers[j];
+                int b = singers[j + 1];
+                graph.get(a).add(b);
+                indegree[b]++;
+            }
+        }
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i < N + 1; i++)
+            if (indegree[i] == 0)
+                queue.offer(i);
+
+        List<Integer> answer = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            answer.add(cur);
+
+            for (int next : graph.get(cur)) {
+                if (--indegree[next] == 0)
+                    queue.offer(next);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (answer.size() == N) {
+            for (int singer : answer)
+                sb.append(singer).append("\n");
+        } else {
+            sb.append(0).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
---
+++ src/baekjoon/unclassified/Main_G3_02473_세용액.java
@@ -0,0 +1,49 @@
+package baekjoon.unclassified;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_G3_02473_세용액 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        long[] arr = new long[N];
+        for (int i = 0; i < N; i++)
+            arr[i] = Long.parseLong(st.nextToken());
+
+        Arrays.sort(arr);
+        long sum = Long.MAX_VALUE;
+        long answer1 = 0, answer2 = 0, answer3 = 0;
+
+        for (int i = 0; i < N - 2; i++) {
+            int pointer1 = i + 1;
+            int pointer2 = N - 1;
+
+            while (pointer1 < pointer2) {
+                long newSum = arr[i] + arr[pointer1] + arr[pointer2];
+
+                if (Math.abs(newSum) < Math.abs(sum)) {
+                    sum = newSum;
+                    answer1 = arr[i];
+                    answer2 = arr[pointer1];
+                    answer3 = arr[pointer2];
+                }
+
+                if (newSum < 0) {
+                    pointer1++;
+                } else {
+                    pointer2--;
+                }
+            }
+        }
+
+        bw.write(answer1 + " " + answer2 + " " + answer3);
+        bw.flush();
+    }
+}
