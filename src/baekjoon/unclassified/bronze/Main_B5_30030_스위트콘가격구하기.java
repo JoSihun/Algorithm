5fe7ed5f3884336b44032ab79441523e0300ebdd
조시훈 <kepler452k@naver.com>
2024-10-03 13:55:53 +0900

    feat: 241003 Algorithm Practice

---
+++ src/baekjoon/unclassified/bronze/Main_B5_30030_스위트콘가격구하기.java
@@ -0,0 +1,12 @@
+package baekjoon.unclassified.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_B5_30030_스위트콘가격구하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) / 11 * 10);
+    }
+}
