9926e143491769e8e2f292c22734697747eb3f5c
조시훈 <kepler452k@naver.com>
2024-12-08 15:36:04 +0900

    feat: 241208 Algorithm Practice

---
+++ src/baekjoon/unclassified/bronze/Main_B4_32025_체육은수학과목입니다.java
@@ -0,0 +1,14 @@
+package baekjoon.unclassified.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_B4_32025_체육은수학과목입니다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int H = Integer.parseInt(br.readLine());
+        int W = Integer.parseInt(br.readLine());
+        System.out.println(Math.min(H, W) * 100 / 2);
+    }
+}
