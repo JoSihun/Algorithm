9bcdd2dccb08d36e54adb99e5d9ce66313f6cb81
조시훈 <kepler452k@naver.com>
2024-12-17 20:34:47 +0900

    feat: 241216 Algorithm Practice

--- src/baekjoon/unclassified/bronze/Main_B4_28224_FinalPrice.java
+++ src/baekjoon/unclassified/bronze/Main_B4_28224_FinalPrice.java
@@ -2,7 +2,6 @@ package baekjoon.unclassified.bronze;
 
 import java.io.*;
 
-// https://www.acmicpc.net/problem/28224
 public class Main_B4_28224_FinalPrice {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
