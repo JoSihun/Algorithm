a4fe75eadfeeb979be9be1fce7375462d958c08c
조시훈 <kepler452k@naver.com>
2025-01-02 17:36:40 +0900

    feat: 250102 Algorithm Practice

---
+++ src/baekjoon/unclassified/bronze/Main_B4_31668_특별한가지.java
@@ -0,0 +1,15 @@
+package baekjoon.unclassified.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_B4_31668_특별한가지 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        System.out.println(M / N * K);
+    }
+}
