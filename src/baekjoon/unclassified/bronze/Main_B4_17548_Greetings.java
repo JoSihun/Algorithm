ea28c7063fcc1167c57b49cce6f38bac405610f4
조시훈 <kepler452k@naver.com>
2025-01-15 21:14:20 +0900

    feat: 250115 Algorithm Practice

---
+++ src/baekjoon/unclassified/bronze/Main_B4_17548_Greetings.java
@@ -0,0 +1,12 @@
+package baekjoon.unclassified.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_B4_17548_Greetings {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().replace("e", "ee"));
+    }
+}
