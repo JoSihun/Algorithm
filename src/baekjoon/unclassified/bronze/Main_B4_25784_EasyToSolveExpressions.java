0e8e8a1d1ce565614a0a56fc9fff9a1375492b6d
조시훈 <kepler452k@naver.com>
2025-01-17 17:12:09 +0900

    feat: 250117 Algorithm Practice

---
+++ src/baekjoon/unclassified/bronze/Main_B4_25784_EasyToSolveExpressions.java
@@ -0,0 +1,18 @@
+package baekjoon.unclassified.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_B4_25784_EasyToSolveExpressions {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        System.out.println((a + b == c || a + c == b || b + c == a) ? 1 :
+                (a * b == c || a * c == b || b * c == a) ? 2 : 3);
+    }
+}
