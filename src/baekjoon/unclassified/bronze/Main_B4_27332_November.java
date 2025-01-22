057cce3e09fbbdab53be68d6ad12424a8e749218
조시훈 <kepler452k@naver.com>
2025-01-22 14:58:42 +0900

    feat: 250122 Algorithm Practice

---
+++ src/baekjoon/unclassified/bronze/Main_B4_27332_November.java
@@ -0,0 +1,14 @@
+package baekjoon.unclassified.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_B4_27332_November {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        System.out.println(A + 7 * B > 30 ? 0 : 1);
+    }
+}
