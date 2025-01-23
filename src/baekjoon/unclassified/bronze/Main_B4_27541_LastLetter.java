d558a6cecf13555f53aab7c5539cc66c9d4cdb29
조시훈 <kepler452k@naver.com>
2025-01-23 12:52:01 +0900

    feat: 250123 Algorithm Practice

---
+++ src/baekjoon/unclassified/bronze/Main_B4_27541_LastLetter.java
@@ -0,0 +1,14 @@
+package baekjoon.unclassified.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_B4_27541_LastLetter {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+        System.out.println(S.endsWith("G") ? S.substring(0, N - 1) : S + "G");
+    }
+}
