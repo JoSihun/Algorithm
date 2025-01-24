2da5fdb39c35c88e708edb71c1c93565540639f7
조시훈 <kepler452k@naver.com>
2025-01-24 19:42:19 +0900

    feat: 250124 Algorithm Practice

---
+++ src/baekjoon/unclassified/bronze/Main_B4_28490_Area.java
@@ -0,0 +1,21 @@
+package baekjoon.unclassified.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_B4_28490_Area {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            answer = Math.max(answer, h * w);
+        }
+        System.out.println(answer);
+    }
+}
