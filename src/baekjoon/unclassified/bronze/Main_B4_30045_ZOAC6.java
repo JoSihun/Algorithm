9d4e2123a75c8a2b7403383c6538facdac26bb8b
조시훈 <kepler452k@naver.com>
2025-01-26 19:56:56 +0900

    feat: 250126 Algorithm Practice

--- src/baekjoon/unclassified/bronze/Main_B4_30045_ZOAC6.java
+++ src/baekjoon/unclassified/bronze/Main_B4_30045_ZOAC6.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// https://www.acmicpc.net/problem/30045
 public class Main_B4_30045_ZOAC6 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
