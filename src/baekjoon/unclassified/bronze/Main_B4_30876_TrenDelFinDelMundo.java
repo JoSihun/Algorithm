6306a974ea77893b6e7b1d13fa78eb71c31a917e
조시훈 <kepler452k@naver.com>
2025-01-28 06:44:59 +0900

    feat: 250128 Algorithm Practice

---
+++ src/baekjoon/unclassified/bronze/Main_B4_30876_TrenDelFinDelMundo.java
@@ -0,0 +1,26 @@
+package baekjoon.unclassified.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_B4_30876_TrenDelFinDelMundo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int ex = Integer.MAX_VALUE, ey = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if (y < ey) {
+                ex = x;
+                ey = y;
+            }
+        }
+        System.out.println(ex + " " + ey);
+    }
+}
+
