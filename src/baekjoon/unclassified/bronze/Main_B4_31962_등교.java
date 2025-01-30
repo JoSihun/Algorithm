e57118fa3a86187a0e5b82a04b8d53092469a9d0
조시훈 <kepler452k@naver.com>
2025-01-31 06:05:57 +0900

    feat: 250131 Algorithm Practice

---
+++ src/baekjoon/unclassified/bronze/Main_B4_31962_등교.java
@@ -0,0 +1,25 @@
+package baekjoon.unclassified.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/31962
+public class Main_B4_31962_등교 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int answer = -1;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int T = Integer.parseInt(st.nextToken());
+            if (S + T <= X) answer = Math.max(answer, S);
+        }
+        System.out.println(answer);
+    }
+}
